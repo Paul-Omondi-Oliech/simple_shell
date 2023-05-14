c079f59e43f6e630d8a3176eceb49c46f8138aff
Paul-Omondi-Oliech <paulomondioyoo@gmail.com>
2023-05-14 12:51:20 -0700

    update builtin2.c

--- builtin2.c
+++ builtin2.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 /**
- * new_cd - Changes the current working directory .
+ * new_cd - Changes the current working directory.
  * if no parameter is passed it will change directory to HOME.
- * @vars: String representing the input from the user.
+ * @vars: String representing the user input.
  */
 void new_cd(vars_t *vars)
 {
